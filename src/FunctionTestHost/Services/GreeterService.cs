d29368ae21a4394ba4a5e0ea59bfc3f72e6c8a75
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2021-11-13 11:07:03 +0000

    Cleanup grpc protos

--- src/FunctionTestHost/Services/GreeterService.cs
+++ src/FunctionTestHost/Services/GreeterService.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AzureFunctionsRpcMessages;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
 namespace FunctionTestHost
 {
+    public class FunServ : FunctionRpc.FunctionRpcBase
+    {
+        public override Task EventStream(IAsyncStreamReader<StreamingMessage> requestStream, IServerStreamWriter<StreamingMessage> responseStream, ServerCallContext context)
+        {
+            return base.EventStream(requestStream, responseStream, context);
+        }
+    }
+    
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
