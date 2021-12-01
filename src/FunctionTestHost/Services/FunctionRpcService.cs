63f88c7d59dab5702e4e439ff6893fff5cfe5f49
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2021-12-01 23:08:48 +0100

    Prototype to call serverless function

--- src/FunctionTestHost/Services/FunctionRpcService.cs
+++ src/FunctionTestHost/Services/FunctionRpcService.cs
@@ -21,6 +21,78 @@ namespace FunctionTestHost
         {
             var functionGrain = await SetupFunctionGrain(requestStream);
             await functionGrain.Init();
+
+            await responseStream.WriteAsync(new StreamingMessage
+            {
+                FunctionLoadRequest = new FunctionLoadRequest
+                {
+                    FunctionId = "Hello",
+                    Metadata = new RpcFunctionMetadata
+                    {
+                        IsProxy = false,
+                        ScriptFile = "FunctionAppOne.dll",
+                        Name = "Hello",
+                        EntryPoint = "FunctionAppOne.Hello.Run",
+                    }
+                }
+            });
+
+            var response = await WaitTillInit(requestStream);
+
+            await responseStream.WriteAsync(new StreamingMessage
+            {
+                InvocationRequest = new InvocationRequest
+                {
+                    FunctionId = "Hello",
+                    InvocationId = "123",
+                    InputData =
+                    {
+                        new ParameterBinding
+                        {
+                            Name = "req",
+                            Data = new TypedData
+                            {
+                                Http = new RpcHttp
+                                {
+                                }
+                            }
+                        }
+                    },
+                    TraceContext = new RpcTraceContext
+                    {
+                        TraceParent = "123"
+                    }
+                }
+            });
+
+            await StartReading(requestStream);
+        }
+
+        private async Task StartReading(IAsyncStreamReader<StreamingMessage> requestStream)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var dd = requestStream.Current;
+                if (dd.InvocationResponse is { } response)
+                {
+                    var gg = response;
+                    return;
+                }
+            }
+        }
+
+        private async Task<FunctionLoadResponse> WaitTillInit(IAsyncStreamReader<StreamingMessage> requestStream)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var nextMessage = requestStream.Current;
+                if (nextMessage.FunctionLoadResponse is { } initRequest)
+                {
+                    return initRequest;
+                }
+            }
+
+            throw new Exception("Expected StartStream message");
         }
 
         private async Task<IFunctionGrain> SetupFunctionGrain(IAsyncStreamReader<StreamingMessage> requestStream)
