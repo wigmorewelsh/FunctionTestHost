28f8e97e5b5ee95f6fc37beda14814d3ae487cbd
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2021-12-07 10:15:07 +0100

    Pass metadata load requests to function app

--- src/FunctionTestHost/Actors/FunctionGrain.cs
+++ src/FunctionTestHost/Actors/FunctionGrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 using AzureFunctionsRpcMessages;
@@ -18,9 +19,9 @@ namespace FunctionTestHost.Actors
             _manager = manager;
             _grpcChannel = manager.Lookup(this.GetPrimaryKeyString());
         }
-        
+
         private FunctionState State = FunctionState.Init;
-        private readonly ChannelWriter<StreamingMessage> _grpcChannel;
+        private readonly ChannelWriter<AzureFunctionsRpcMessages.StreamingMessage> _grpcChannel;
 
         public Task Init()
         {
@@ -30,12 +31,31 @@ namespace FunctionTestHost.Actors
 
         public async Task InitMetadata(StreamingMessage message)
         {
-            await _grpcChannel.WriteAsync(message);
+            foreach (var loadRequest in message.FunctionInit.FunctionLoadRequestsResults)
+            {
+                await _grpcChannel.WriteAsync(new AzureFunctionsRpcMessages.StreamingMessage
+                {
+                    RequestId = Guid.NewGuid().ToString(),
+                    FunctionLoadRequest = new FunctionLoadRequest
+                    {
+                        FunctionId = loadRequest.FunctionId,
+                        ManagedDependencyEnabled = loadRequest.ManagedDependencyEnabled,
+                        Metadata = new RpcFunctionMetadata
+                        {
+                            Name = loadRequest.Metadata.Name,
+                            Directory = loadRequest.Metadata.Directory,
+                            EntryPoint = loadRequest.Metadata.EntryPoint,
+                            IsProxy = loadRequest.Metadata.IsProxy,
+                            ScriptFile = loadRequest.Metadata.ScriptFile
+                        }
+                    }
+                });
+            }
         }
 
         public Task Notification()
         {
-            throw new System.NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
