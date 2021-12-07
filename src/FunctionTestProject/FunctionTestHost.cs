e7de32a0627362b0f78006501ce4cbd193239f8d
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2021-12-07 10:29:28 +0100

    Extract test host into seperate class

---
+++ src/FunctionTestProject/FunctionTestHost.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FunctionAppOne;
+using FunctionMetadataEndpoint;
+using FunctionTestHost;
+using FunctionTestHost.Actors;
+using FunctionTestHost.MetadataClient;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Orleans;
+using Orleans.Hosting;
+using Xunit;
+
+namespace FunctionTestProject
+{
+    public class FunctionTestHost : IAsyncDisposable, IAsyncLifetime
+    {
+        private AsyncLock _lock = new();
+        private volatile bool _isInit = false;
+
+        private IHost _fakeHost;
+        private IHost _functionHost;
+        private const int Port = 20222;
+
+        public async Task CreateServer()
+        {
+            if(_isInit) return;
+            using var _ = await _lock.LockAsync();
+            if(_isInit) return;
+
+            _fakeHost = Host.CreateDefaultBuilder()
+                .UseOrleans(orleans =>
+                {
+                    orleans.UseLocalhostClustering();
+                    orleans.ConfigureApplicationParts(parts =>
+                        parts.AddApplicationPart(typeof(FunctionGrain).Assembly));
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(k =>
+                        k.ListenLocalhost(Port, opt => opt.Protocols = HttpProtocols.Http2)).UseStartup<Startup>();
+                })
+                .Build();
+
+            _fakeHost.Start();
+
+            _functionHost = new HostBuilder()
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        ["Host"] = "localhost",
+                        ["Port"] = Port.ToString(),
+                        ["WorkerId"] = "123",
+                        ["GrpcMaxMessageLength"] = "1024"
+                    });
+                })
+                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices((host, services) =>
+                {
+                    services.Configure<GrpcWorkerStartupOptions>(host.Configuration);
+                    services.AddSingleton(ctx =>
+                    {
+                        var options = ctx.GetRequiredService<IOptions<GrpcWorkerStartupOptions>>();
+                        var url = new Uri($"http://{options.Value.Host}:{options.Value.Port}");
+                        var channel = GrpcChannel.ForAddress(url, new GrpcChannelOptions());
+                        return new FunctionRpc.FunctionRpcClient(channel);
+                    });
+                    services.AddHostedService<MetadataClientRpc<Hello>>();
+                })
+                .Build();
+
+            _functionHost.Start();
+            _isInit = true;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await CreateServer();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await ((IAsyncDisposable)this).DisposeAsync();
+        }
+
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            await _functionHost.StopAsync();
+            await _fakeHost.StopAsync();
+        }
+    }
+}
--- src/FunctionTestProject/UnitTest1.cs
+++ src/FunctionTestProject/UnitTest1.cs
@@ -1,79 +1,22 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using FunctionAppOne;
-using FunctionMetadataEndpoint;
-using FunctionTestHost;
-using FunctionTestHost.Actors;
-using FunctionTestHost.MetadataClient;
 using Grpc.Core;
-using Grpc.Net.Client;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Orleans;
-using Orleans.Hosting;
 using Xunit;
 
 namespace FunctionTestProject
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<FunctionTestHost>
     {
-        private const int Port = 20222;
+        private readonly FunctionTestHost _testHost;
+
+        public UnitTest1(FunctionTestHost testHost)
+        {
+            _testHost = testHost;
+        }
 
         [Fact]
         public async Task Test1()
         {
-            using var fakeHost = Host.CreateDefaultBuilder()
-                .UseOrleans(orleans =>
-                {
-                    orleans.UseLocalhostClustering();
-                    orleans.ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(FunctionGrain).Assembly));
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(k =>
-                        k.ListenLocalhost(Port, opt => opt.Protocols = HttpProtocols.Http2)).UseStartup<Startup>();
-                })
-                .Build();
-
-            fakeHost.Start();
-
-            using var functionHost = new HostBuilder()
-                .ConfigureAppConfiguration(config =>
-                {
-                    config.AddInMemoryCollection(new Dictionary<string, string>
-                    {
-                        ["Host"] = "localhost",
-                        ["Port"] = Port.ToString(),
-                        ["WorkerId"] = "123",
-                        ["GrpcMaxMessageLength"] = "1024"
-                    });
-                })
-                .ConfigureFunctionsWorkerDefaults()
-                .ConfigureServices((host, services ) =>
-                {
-                    services.Configure<GrpcWorkerStartupOptions>(host.Configuration);
-                    services.AddSingleton(ctx =>
-                    {
-                        var options = ctx.GetRequiredService<IOptions<GrpcWorkerStartupOptions>>();
-                        var url = new Uri($"http://{options.Value.Host}:{options.Value.Port}");
-                        var channel = GrpcChannel.ForAddress(url, new GrpcChannelOptions());
-                        return new FunctionRpc.FunctionRpcClient(channel);
-                    });
-                    services.AddHostedService<MetadataClientRpc<Hello>>();
-                })
-                .Build();
-
-            functionHost.Start();
-
-            await Task.Delay(30_000);
-
-            await functionHost.StopAsync();
-            await fakeHost.StopAsync();
+            await _testHost.CreateServer();
         }
     }
 }
