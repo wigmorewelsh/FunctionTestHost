0251844034820a4fe72ecf92cac782214932bde0
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2023-06-09 16:34:16 +0100

    Add more docs

--- src/TestKit/TestHost/FunctionTestApp.cs
+++ src/TestKit/TestHost/FunctionTestApp.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace TestKit.TestHost;
 
+/// <summary>
+/// A function app represents a azure function.
+/// </summary>
 public interface FunctionTestApp : IAsyncDisposable
 {
     Task Start();
