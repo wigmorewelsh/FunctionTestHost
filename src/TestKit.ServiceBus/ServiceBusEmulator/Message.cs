c3eb4480cc9fc5c60397bce1785b15b29587386e
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2024-01-02 21:27:37 +0000

    Add converters for amqp messages

--- src/TestKit.ServiceBus/ServiceBusEmulator/Message.cs
+++ src/TestKit.ServiceBus/ServiceBusEmulator/Message.cs
@@ -1,6 +1,12 @@
 namespace TestKit.ServiceBus.ServiceBusEmulator;
 
+#if NET7_0_OR_GREATER
+[GenerateSerializer]
+#endif
 public class Message
 {
+    #if NET7_0_OR_GREATER
+    [Id(0)]
+    #endif
     public int Tag { get; }
 }
